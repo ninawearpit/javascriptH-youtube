5e212459533c67fd0014cc7bc5cdf2576c9aabd3
ninawearpit <arpitninawe706@gmail.com>
2025-01-21 06:38:42 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 14553
+var accountPassword = "12345"
+let accountEmail = "ninja2@gmail.com"
+accountCity = "Nagpur"
+
+
+
+accountEmail ="arpit12@gmail.com"
+accountPassword = "5678"
+accountCity ="pune"
+
+
+console.table([accountEmail, accountId, accountPassword, accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log(3+3)
+console.log("Arpit")
+
+
+console.log(typeof 12346 )
